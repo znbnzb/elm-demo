71ab5a78db447f6c6b6f2137bb45e5dd5b974922
lianxinagPC <1524922080@qq.com>
2018-11-08 19:59:25 +0800

    获取数据

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -75,6 +75,12 @@ export default async (url = '', data = {}, type = 'GET', method = 'fatch') => {
       throw new Error(error)
     }
   } else {
+    /**
+     * Promise的构造函数接受一个参数， 是函数；
+     *  并且传入两个参数： resolve, reject，
+     *  分别表示异步操作执行成功后的回调函数和异步操作执行失败后的回调函数；
+     */
+
     return new Promise((resolve, reject) => {
       let requestObj; //ajax请求方法
       if (window.XMLHttpRequest) {
