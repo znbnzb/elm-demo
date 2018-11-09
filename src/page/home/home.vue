4fbc1b42fab2d2c51e48e1380dcffd6a8a284bbd
lianxinagPC <1524922080@qq.com>
2018-11-09 22:26:25 +0800

    完成home页面

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,26 +1,198 @@
 <template>
     <div>
-        <p>呆呆{{guessCity}}</p>
+        <!-- 头部组件 -->
+        <head-top signin-up='home'>
+            <span slot='logo' class="head_logo" @click="reload">饿了么</span>
+        </head-top>
+
+        <!-- 当前城市定位 -->
+        <nav class="city_nav">
+
+            <!-- 定位提醒 -->
+            <div class="city_tip">
+                <span>当前定位城市</span>
+                <span>定位不准时，请在城市列表中选择</span>
+            </div>
+
+            <!-- 当前定位的城市 -->
+            <router-link :to="'/city/' + guessCityid" class="guess_city">
+                <span>{{guessCity}}</span>
+                <svg class="arrow_right">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                </svg>
+            </router-link>
+        </nav>
+
+        <!-- 热门城市 -->
+        <section id="hot_city_container">
+            <h4 class="city_title">热门城市</h4>
+            <ul class='citylistul clear'>
+                <router-link tag="li" v-for="item in hotcity" :to="'/city' + item.id" :key="item.id">
+                    {{item.name}}
+                </router-link>
+            </ul>
+        </section>
+
+        <!-- 所有城市 -->
+        <section class="group_city_container">
+            <ul class="letter_classfify">
+                <li v-for="(value,key,index) in sortgroupcity" :key="key" class="letter_classify_li">
+                    <h4 class="city_title">{{key}}
+                        <span v-if="index == 0">(按字母排序)</span>
+                    </h4>
+                    <ul class="groupcity_name_container citylistul clear">
+                        <router-link tag="li" v-for="item in value" :to="'/city/' + item.id" :key="item.id" class="ellipsis">
+                            {{item.name}}
+                        </router-link>
+                    </ul>
+                </li>
+            </ul>
+        </section>
     </div>
 </template>
 
 <script>
-import { cityGuess } from "../../service/getDate";
+import headTop from "../../components/header/head";
+import { cityGuess, hotcity, groupcity } from "../../service/getDate";
 export default {
   data() {
     return {
-      guessCity: ""
+      guessCity: "", //当前城市-福州
+      hotcity: [], //热门城市列表
+      guessCityid: "", //当前城市id-福州
+      groupcity: {} //所有城市列表
     };
   },
   mounted() {
     //获取当前城市
     cityGuess().then(res => {
       this.guessCity = res.name;
-      console.log(this.guessCity);
+      this.guessCityid = res.id;
     });
+
+    //获取热门城市
+    hotcity().then(res => {
+      this.hotcity = res;
+    });
+
+    //获取所有城市列表
+    groupcity().then(res => {
+      this.groupcity = res;
+    });
+  },
+  components: {
+    headTop
+  },
+  computed: {
+    //将获取的数据按照A-Z字母开头排序
+    sortgroupcity() {
+      let sortobj = {};
+      for (let i = 65; i <= 90; i++) {
+        if (this.groupcity[String.fromCharCode(i)]) {
+          sortobj[String.fromCharCode(i)] = this.groupcity[
+            String.fromCharCode(i)
+          ];
+        }
+      }
+      return sortobj;
+    }
+  },
+  methods: {
+    //点击图标刷新页面
+    reload() {
+      window.location.reload();
+    }
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "../../style/mixin";
+.head_logo {
+  left: 0.4rem;
+  font-weight: 400;
+  @include sc(0.7rem, #fff);
+  @include wh(2.3rem, 0.7rem);
+  @include ct;
+}
+.city_nav {
+  padding-top: 2.35rem;
+  border-top: 1px solid $bc;
+  background-color: #fff;
+  margin-bottom: 0.4rem;
+  //   当前城市定位提示
+  .city_tip {
+    @include fj;
+    line-height: 1.45rem;
+    padding: 0 0.45rem;
+    span:nth-of-type(1) {
+      @include sc(0.55rem, #666);
+    }
+    span:nth-of-type(2) {
+      font-weight: 900;
+      @include sc(0.475rem, #9f9f9f);
+    }
+  }
+  //当前城市定位
+  .guess_city {
+    @include fj;
+    align-items: center;
+    height: 1.8rem;
+    padding: 0 0.45rem;
+    border-top: 1px solid $bc;
+    border-bottom: 2px solid $bc;
+    @include font(0.75rem, 1.8rem);
+    span:nth-of-type(1) {
+      color: $blue;
+    }
+    .arrow_right {
+      @include wh(0.6rem, 0.6rem);
+      fill: #999;
+    }
+  }
+}
+// 热门城市-最外框
+#hot_city_container {
+  background-color: #fff;
+  margin-bottom: 0.4rem;
+}
+//热门城市的每一个li
+.citylistul {
+  li {
+    float: left;
+    text-align: center;
+    color: $blue;
+    border-bottom: 0.025rem solid $bc;
+    border-right: 0.025rem solid $bc;
+    @include wh(25%, 1.75rem);
+    @include font(0.6rem, 1.75rem);
+  }
+  li:nth-of-type(4n) {
+    border-right: none;
+  }
+}
+// 热门城市标题
+.city_title {
+  color: #666;
+  font-weight: 400;
+  text-indent: 0.45rem;
+  border-top: 2px solid $bc;
+  border-bottom: 1px solid $bc;
+  @include font(0.55rem, 1.45rem, "Helvetica Neue");
+  span {
+    @include sc(0.475rem, #999);
+  }
+}
+
+//所有城市
+.letter_classify_li {
+  margin-bottom: 0.4rem;
+  background-color: #fff;
+  border-bottom: 1px solid $bc;
+  .groupcity_name_container {
+    li {
+      color: #666;
+    }
+  }
+}
 </style>
