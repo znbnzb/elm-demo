d843f4b046db696f21b3feb1d55b4efca72fc6fa
lianxinagPC <1524922080@qq.com>
2018-11-18 21:36:25 +0800

    无法启动

--- src/page/msite/msite.vue
+++ src/page/msite/msite.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        <shoplist></shoplist>
-        <!-- 底部组件 -->
-        <footGuide></footGuide>
-    </div>
+  <div>
+    <shoplist></shoplist>
+    <!-- 底部组件 -->
+    <footGuide></footGuide>
+  </div>
 </template>
 
 <script>
-// 底部组件
-import footGuide from "../../components/footer/footerGuide";
-import shoplist from "../../components/common/shoplist";
+import { mapMutations } from "vuex";
+import headTop from "../../components/header/head"; //头部组件
+import footGuide from "../../components/footer/footerGuide"; // 底部组件
+import shoplist from "../../components/common/shoplist"; //商铺组件
+import { msiteAddress, msiteFoodTypes, cityGuess } from "../../service/getDate";
+// import "../../plugins/swiper.min.js";
+// import "../../style/swiper.min";
+
 export default {
   components: {
     footGuide,
